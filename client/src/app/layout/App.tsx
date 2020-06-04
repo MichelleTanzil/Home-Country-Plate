df7d752238ab605bfecb7b980a634ce77dadadec
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-04 06:48:32 -0700

    Added notfound component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,28 +1,38 @@
 import React from "react";
 import { HomePage } from "../../features/home/HomePage";
-import { Navbar } from '../../features/navbar/Navbar';
-import { Route, Switch, RouteComponentProps, withRouter } from "react-router-dom";
+import { Navbar } from "../../features/navbar/Navbar";
+import {
+  Route,
+  Switch,
+  RouteComponentProps,
+  withRouter,
+} from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import ProductList from "../../features/products/dashboard/ProductList";
 import { ProductDetails } from "../../features/products/dashboard/ProductDetails";
+import NotFound from "./NotFound";
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <>
       <Navbar />
       <Route exact path="/" component={HomePage} />
-      <Route path={'/(.+)'} render={() => (
-        <>
-          <Container style={{ marginTop: '7em' }}>
-            <Switch>
-              <Route exact path="/products" component={ProductList} />
-              <Route path="/products/:id" component={ProductDetails} />
-            </Switch>
-          </Container>
-        </>
-      )} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <Container style={{ marginTop: "7em" }}>
+              <Switch>
+                <Route exact path="/products" component={ProductList} />
+                <Route path="/products/:id" component={ProductDetails} />
+                <Route component={NotFound} />
+              </Switch>
+            </Container>
+          </>
+        )}
+      />
     </>
   );
-}
+};
 
 export default withRouter(App);
--- client/src/features/products/dashboard/ProductDetails.tsx
+++ client/src/features/products/dashboard/ProductDetails.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import React from "react";
+import { Card, Icon, Image } from "semantic-ui-react";
 
 export const ProductDetails = () => {
   return (
     <Card fluid>
-      <Image src={'/assets/foodPlaceholder.png'} wrapped ui={false} />
+      <Image
+        src={"/assets/foodPlaceholder.png"}
+        wrapped
+        ui={false}
+        size="medium"
+      />
       <Card.Content>
         <Card.Header>Title</Card.Header>
         <Card.Meta>
-          <span className='date'>Joined in 2015</span>
+          <span className="date">Joined in 2015</span>
         </Card.Meta>
         <Card.Description>
           Matthew is a musician living in Nashville.
-      </Card.Description>
+        </Card.Description>
       </Card.Content>
       <Card.Content extra>
-        <a>
-          <Icon name='user' />
-        22 Friends
-      </a>
+        <a href={"/products"}>
+          <Icon name="user" />
+          22 Friends
+        </a>
       </Card.Content>
     </Card>
-  )
-}
+  );
+};

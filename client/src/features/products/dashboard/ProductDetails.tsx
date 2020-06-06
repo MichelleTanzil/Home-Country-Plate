5953988da8fc1711f169881f05969d62202f9cad
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-06 10:44:38 -0700

    Completed ProductDetails component

--- client/src/features/products/dashboard/ProductDetails.tsx
+++ client/src/features/products/dashboard/ProductDetails.tsx
@@ -1,5 +1,15 @@
 import React, { useContext, useEffect } from "react";
-import { Card, Icon, Image, Grid, GridColumn, Button } from "semantic-ui-react";
+import {
+  Header,
+  Card,
+  Icon,
+  Image,
+  Grid,
+  GridColumn,
+  Button,
+  Segment,
+  Divider,
+} from "semantic-ui-react";
 import ProductStore from "../../../app/stores/productStore";
 import { RouteComponentProps } from "react-router-dom";
 import { observer } from "mobx-react-lite";
@@ -26,31 +36,70 @@ const ProductDetails: React.FC<RouteComponentProps<DetailParams>> = ({
     );
   return (
     <Grid>
-      <GridColumn width={8}>
-        <Image src={"/assets/foodPlaceholder.png"} wrapped size="medium" />
-        <Button animated="vertical" color="teal" size="large">
+      <Grid.Column width={8}>
+        <Image src={"/assets/foodPlaceholder.png"} wrapped size="large" />
+      </Grid.Column>
+      <Grid.Column width={8}>
+        <Segment fluid>
+          <Header as="h1" textAlign="center">
+            {product.title}
+          </Header>
+          <Divider horizontal>
+            <Header as="h4">
+              <Icon name="tag" />
+              Description
+            </Header>
+          </Divider>
+          <p>{product.description}</p>
+          <Divider horizontal>
+            <Header as="h4">
+              <Icon name="food" />
+              Tags
+            </Header>
+          </Divider>
+          <p>Vegan, Dairy Free, Contains Nuts</p>
+        </Segment>
+        <Button animated="vertical" color="teal" fluid size="large" relaxed>
           <Button.Content hidden>Add to cart</Button.Content>
           <Button.Content visible>
             <Icon name="shop" size="large" />
           </Button.Content>
         </Button>
-      </GridColumn>
-      <GridColumn width={8}></GridColumn>
-      <Card fluid>
-        <Card.Content>
-          <Card.Header>{product.title}</Card.Header>
-          <Card.Meta>
-            <span className="date">Joined in 2015</span>
-          </Card.Meta>
-          <Card.Description>{product.description}</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a href={"/products"}>
-            <Icon name="user" />
-            22 Friends
-          </a>
-        </Card.Content>
-      </Card>
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <Card>
+          {/* Link to user's profile */}
+          <Image src="/assets/userPlaceholder.png" fluid size="small" />
+          <Card.Content>
+            <Card.Header>Anonymous</Card.Header>
+            <Card.Meta>
+              <span className="date">Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Anonymous is a cook who has been cooking for decades specializing
+              in burgers.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="user" />
+              22 Following
+            </a>
+          </Card.Content>
+          <Card.Content extra>
+            <a>
+              <Icon name="thumbs up" />
+              11 Liked
+            </a>
+          </Card.Content>{" "}
+          <Card.Content extra>
+            <a>
+              <Icon name="food" />
+              150 dishes sold
+            </a>
+          </Card.Content>
+        </Card>
+      </Grid.Column>
     </Grid>
   );
 };

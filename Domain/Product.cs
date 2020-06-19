b00de98664580d8b6248e79be41e45a40e73ddd8
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-18 20:47:19 -0700

    Established UserProduct relationship

--- Domain/Product.cs
+++ Domain/Product.cs
@@ -1,14 +1,18 @@
 using System;
+using System.Collections.Generic;
 
-namespace Domain {
-    public class Product {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Image { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public float Price { get; set; }
-    }
+namespace Domain
+{
+  public class Product
+  {
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public float Price { get; set; }
+    public ICollection<UserProduct> UserProducts { get; set; }
+  }
 }

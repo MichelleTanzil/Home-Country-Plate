df5b1ace01bdb69eb6839c24e8c4aa8c47fb182f
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-29 15:36:16 -0700

    Domain entity, db relationship and migrations for Photos

--- Domain/AppUser.cs
+++ Domain/AppUser.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
-namespace Domain {
-  public class AppUser : IdentityUser {
+namespace Domain
+{
+  public class AppUser : IdentityUser
+  {
     public string DisplayName { get; set; }
     public string Bio { get; set; }
     public virtual ICollection<UserProduct> UserProducts { get; set; }
+    public virtual ICollection<Photo> UserPhotos { get; set; } //1:m relationship with Photos
     public virtual Cart UserCart { get; set; } = null; // 1:1 relationship with the cart
     public virtual ICollection<Order> UserOrders { get; set; }
   }

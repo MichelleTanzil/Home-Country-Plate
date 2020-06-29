21deb361e16c8dbb22f05d7057ee46cc1dbf3650
Michelle Tanzil <mtanzil01@gmail.com>
2020-06-29 10:58:35 -0700

    Added AddPhoto handler

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,14 +1,45 @@
 using Application.Interfaces;
 using Application.Photos;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Photos
 {
   public class PhotoAccessor : IPhotoAccessor
   {
-    public PhotoUploadResult AddPhoto(IFormFile File)
+    private readonly Cloudinary _cloudinary;
+    public PhotoAccessor(IOptions<CloudinarySettings> config) //allows us to access user secrets and is strongly typed
     {
-      throw new System.NotImplementedException();
+      var acc = new Account(
+        config.Value.CloudName,
+        config.Value.ApiKey,
+        config.Value.ApiSecret
+      );
+
+      _cloudinary = new Cloudinary(acc);
+    }
+
+    public PhotoUploadResult AddPhoto(IFormFile file)
+    {
+      var uploadResult = new ImageUploadResult();
+      if (file.Length > 0)
+      {
+        using (var stream = file.OpenReadStream())
+        {
+          var uploadParams = new ImageUploadParams
+          {
+            File = new FileDescription(file.FileName, stream)
+          };
+          uploadResult = _cloudinary.Upload(uploadParams);
+        }
+      }
+      return new PhotoUploadResult
+      {
+        PublicId = uploadResult.PublicId,
+        Url = uploadResult.SecureUrl.AbsoluteUri
+      };
     }
 
     public string DeletePhoto(string publicId)

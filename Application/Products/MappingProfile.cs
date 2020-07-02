d0cd8b64285e9557097acccdbb61913fa26d35ce
Michelle Tanzil <mtanzil01@gmail.com>
2020-07-01 20:21:07 -0700

    Configure login, register, current user and mapping profile with images

--- Application/Products/MappingProfile.cs
+++ Application/Products/MappingProfile.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using Domain;
 
@@ -11,7 +12,8 @@ namespace Application.Products
       CreateMap<Product, ProductDto>();
       CreateMap<UserProduct, LikesDto>()
         .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
-        .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
+        .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
+        .ForMember(d => d.Image, o => o.MapFrom(s => s.AppUser.UserPhotos.FirstOrDefault(x => x.IsMain).Url));
     }
   }
 }
